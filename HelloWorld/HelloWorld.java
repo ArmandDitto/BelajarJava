fe1ce59f6abc855f5571d74f1817de5ad72a80d5
Muhamad Armanditto <deepfreeze1197@gmail.com>
2019-12-05 12:46:04 +0700

    Menambah class (Blueprint) mobil

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,5 +1,17 @@
 public class HelloWorld{
     public static void main(String[] args){
         System.out.println("Hello World !");
+        mobil Toyota = new mobil();
+        Toyota.merk = "Toyota Kijang";
+        Toyota.maju();
+    }
+    
+}
+
+class mobil{
+    public String merk;
+
+    public void maju(){
+        System.out.println("Mobil "+this.merk+" maju GOOO");
     }
 }
