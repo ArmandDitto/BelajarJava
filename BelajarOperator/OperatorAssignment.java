804fdfa80a6872b1f72d42a64a313bf2125c843c
Muhamad Armanditto <deepfreeze1197@gmail.com>
2019-12-09 12:37:16 +0700

    Menambahkan file Operator Assignment

---
+++ BelajarOperator/OperatorAssignment.java
@@ -0,0 +1,21 @@
+public class OperatorAssignment{
+    public static void main (String[] args){
+        int a = 10;
+        int b = 5;
+        System.out.println(a); //a=10
+        a=b; 
+        System.out.println(a); //a=5
+        a+=b;
+        System.out.println(a); //a=10
+        b-=a;
+        System.out.println(b); //b=-5
+        b*=b;
+        System.out.println(b); //b=25
+        b/=a;
+        System.out.println(b); //b=2
+        a%=b;
+        System.out.println(a); //a=0
+
+
+    }
+}
