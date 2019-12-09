cd5b0a7e9cf9a0e0839653f24b9be5704287a0d9
Muhamad Armanditto <deepfreeze1197@gmail.com>
2019-12-09 10:31:10 +0700

    Tambah TukarNilai.java

---
+++ BelajarVariabel/TukarNilai2.java
@@ -0,0 +1,13 @@
+public class TukarNilai2{
+    public static void main (String[] args){
+        int A = 1;
+        int B = 2;
+
+        A=A+B;
+        B=A-B;
+        A=A-B;
+
+        System.out.println ("A isinya " +A);
+        System.out.println ("B isinya " +B);
+    }
+}
