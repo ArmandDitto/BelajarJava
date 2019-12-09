9f8af9ab314ef529d6c408e797e09d40e4f75f58
Muhamad Armanditto <deepfreeze1197@gmail.com>
2019-12-09 11:45:48 +0700

    Menambahkan OperatorAritmatika.java

---
+++ BelajarOperator/OperatorAritmatika.java
@@ -0,0 +1,21 @@
+public class OperatorAritmatika{
+    public static void main (String[] args){
+        int a = 7;
+        int b = 5;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b); 
+        System.out.println(a%b);
+       
+        System.out.println(++a);
+        System.out.println(a);
+        b++;
+        System.out.println(b);
+
+        /*System.out.println(a++);
+        System.out.println(a);*/ 
+
+    }
+}
