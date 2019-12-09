ec12ad569a05e2856913f4153a3d45dafed96e39
Muhamad Armanditto <deepfreeze1197@gmail.com>
2019-12-09 10:32:05 +0700

    Tambah Biodata.java

---
+++ BelajarVariabel/Biodata.java
@@ -0,0 +1,16 @@
+public class Biodata{
+    public static void main (String[] args){
+        String nama = "Muhamad Armanditto"; 
+        String tempatlahir = "Jakarta";
+        int tanggallahir = 11;
+        int bulanlahir = 11;
+        int tahunlahir = 1997;
+        double umur = 22;
+        String alamat = "Griya Ciledug Blok O no. 11";
+
+        System.out.println ("Nama: "+nama);
+        System.out.println ("Tempat, Tanggal Lahir: " +tempatlahir +", " +tanggallahir +"-" +bulanlahir +"-" +tahunlahir);
+        System.out.println ("Usia: "+umur);
+        System.out.println ("Alamat: "+alamat);
+    }
+}
