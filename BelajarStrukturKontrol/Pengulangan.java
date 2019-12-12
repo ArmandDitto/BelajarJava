ca5bff061989941b0441728ade91c34cc0a70a8e
Muhamad Armanditto <deepfreeze1197@gmail.com>
2019-12-12 20:52:28 +0700

    Menambah file Perulangan for (Pengulangan.java)

---
+++ BelajarStrukturKontrol/Pengulangan.java
@@ -0,0 +1,21 @@
+public class Pengulangan{
+    public static void main (String [] args){
+        /* System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5"); */
+
+        /* for(int jalan=1; jalan<=5; jalan++){
+            System.out.println("Jalan ke-"+jalan);
+        } */
+
+        /* for(int bil=1; bil<=100; bil++){
+            System.out.println(bil);
+        } */
+
+        for(int bil=100; bil>=1; bil--){
+            System.out.println(bil);
+        }
+    }
+}
