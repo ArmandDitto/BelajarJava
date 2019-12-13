4c1a04f70f631793b80504c15347ab91facf3ba5
Muhamad Armanditto <deepfreeze1197@gmail.com>
2019-12-13 10:21:51 +0700

    Penambahan pengulangan ForEach.java

---
+++ BelajarStrukturKontrol/PengulanganForEach.java
@@ -0,0 +1,9 @@
+public class PengulanganForEach{
+    public static void main (String [] args){
+        int[]datas={1,3,7,8};
+
+        for(int x: datas){
+            System.out.println(x);
+        }
+    }
+}
